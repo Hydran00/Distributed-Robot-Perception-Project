eb6912cae99daa795a2350bb18219cc881f6d204
Nardi <davide.nardi-1@studenti.unitn.it>
2024-04-12 14:59:57 +0200

    Minor

--- src/pointcloud_accumulator.cpp
+++ src/pointcloud_accumulator.cpp
@@ -2,6 +2,7 @@
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/random_sample.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <tf2_ros/buffer.h>
@@ -10,20 +11,25 @@
 #include <pcl_ros/transforms.hpp>
 // eigen
 #include <Eigen/Core>
-
-class PointCloudFilterNode : public rclcpp::Node
+using namespace std::chrono_literals;
+class PointCloudAccumulator : public rclcpp::Node
 {
 public:
-    PointCloudFilterNode() : Node("point_cloud_filter_node")
+    PointCloudAccumulator() : Node("point_cloud_filter_node")
     {
+        // avoid storing the pointcloud if transform is not available
+        rclcpp::sleep_for(2s);
+
         // Initialize the filtered point cloud
         total_cloud_ = pcl::PointCloud<pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
 
         // Set maximum size for the filtered point cloud
-        max_cloud_size_ = 10000; // Maximum number of points allowed
 
         this->declare_parameter("voxel_grid_size", 0.01);
         voxel_grid_size = (float)this->get_parameter("voxel_grid_size").as_double();
+
+        this->declare_parameter("max_cloud_size", 10000);
+        max_cloud_size_ = this->get_parameter("max_cloud_size").as_int();
 
         this->declare_parameter("input_topic_1", "/cloud_out_1");
         input_topic_1_ = this->get_parameter("input_topic_1").as_string();
@@ -39,10 +45,10 @@ public:
 
         // Subscribe to the point cloud topic
         point_cloud_subscriber_1_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-            input_topic_1_, 1, std::bind(&PointCloudFilterNode::pointCloudCallback, this, std::placeholders::_1));
+            input_topic_1_, 1, std::bind(&PointCloudAccumulator::pointCloudCallback, this, std::placeholders::_1));
 
         point_cloud_subscriber_2_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-            input_topic_2_, 1, std::bind(&PointCloudFilterNode::pointCloudCallback, this, std::placeholders::_1));
+            input_topic_2_, 1, std::bind(&PointCloudAccumulator::pointCloudCallback, this, std::placeholders::_1));
 
         // Advertise the filtered point cloud topic
         total_cloud_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
@@ -50,6 +56,7 @@ public:
 
         // Initialize the TF2 transform listener and buffer
         tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
+        tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
         // log every param
         RCLCPP_INFO(this->get_logger(), "input_topic_1: %s", input_topic_1_.c_str());
@@ -57,6 +64,9 @@ public:
         RCLCPP_INFO(this->get_logger(), "output_topic: %s", output_topic_.c_str());
         RCLCPP_INFO(this->get_logger(), "total_cloud_frame: %s", total_cloud_frame_.c_str());
         RCLCPP_INFO(this->get_logger(), "voxel_grid_size: %f", voxel_grid_size);
+        RCLCPP_INFO(this->get_logger(), "max_cloud_size: %d", max_cloud_size_);
+
+        RCLCPP_INFO(this->get_logger(), "PointCloudAccumulator has been started.");
     }
 
 private:
@@ -66,8 +76,6 @@ private:
         bool res = transformPointCloud(total_cloud_frame_, *msg, *msg, *tf_buffer_);
         if (!res)
         {
-            RCLCPP_ERROR(this->get_logger(), "Failed to transform point cloud from %s to %s",
-                         msg->header.frame_id.c_str(), total_cloud_frame_.c_str());
             return;
         }
 
@@ -99,27 +107,22 @@ private:
             out = in;
             return true;
         }
-
         try
         {
-            // call without timeout
-            transform_ =
-                tf_buffer.lookupTransform(
-                    target_frame, in.header.frame_id, tf2_ros::fromMsg(in.header.stamp));
+            transform_ = tf_buffer.lookupTransform("world", "1_camera", tf2::TimePointZero);
         }
-        catch (tf2::LookupException &e)
+        catch (tf2::TransformException &ex)
         {
-            RCLCPP_ERROR(rclcpp::get_logger("pcl_ros"), "%s", e.what());
-            return false;
-        }
-        catch (tf2::ExtrapolationException &e)
-        {
-            RCLCPP_ERROR(rclcpp::get_logger("pcl_ros"), "%s", e.what());
+            RCLCPP_ERROR(this->get_logger(), "Failed to get transform: %s", ex.what());
             return false;
         }
 
         // Convert the TF transform to Eigen format
         pcl_ros::transformAsMatrix(transform_, eigen_transform);
+        // print transform
+        // std::cout << "Transform: " << std::endl;
+        // std::cout << eigen_transform << std::endl;
+
         pcl_ros::transformPointCloud(eigen_transform, in, out);
         out.header.frame_id = target_frame;
         return true;
@@ -127,24 +130,34 @@ private:
 
     void filterPointCloud()
     {
-        if (total_cloud_->size() > max_cloud_size_)
+        RCLCPP_INFO(this->get_logger(), "Size is %d", (int)total_cloud_->size());
+        if ((int)total_cloud_->size() > max_cloud_size_)
         {
             // Apply VoxelGrid filter
-            voxel_filter_.setInputCloud(total_cloud_);
-            voxel_filter_.setLeafSize(voxel_grid_size, voxel_grid_size, voxel_grid_size);
-            voxel_filter_.filter(*total_cloud_);
+            // voxel_filter_.setInputCloud(total_cloud_);
+            // voxel_filter_.setLeafSize(voxel_grid_size, voxel_grid_size, voxel_grid_size);
+            // voxel_filter_.filter(*total_cloud_);
+
+            // random sampling filter
+            random_sample.setInputCloud(total_cloud_);
+            random_sample.setSample(max_cloud_size_);
         }
     }
 
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr total_cloud_;
-    size_t max_cloud_size_;
+    int max_cloud_size_;
     float voxel_grid_size;
     std::string input_topic_1_, input_topic_2_, output_topic_, total_cloud_frame_;
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr point_cloud_subscriber_1_, point_cloud_subscriber_2_;
+    rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr total_cloud_publisher_;
-    pcl::VoxelGrid<pcl::PointXYZRGB> voxel_filter_;
     std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
     sensor_msgs::msg::PointCloud2 total_cloud_msg_;
+
+    // filters
+    pcl::VoxelGrid<pcl::PointXYZRGB> voxel_filter_;
+    pcl::RandomSample<pcl::PointXYZRGB> random_sample;
 
     // Get the TF transform
     geometry_msgs::msg::TransformStamped transform_;
@@ -154,7 +167,7 @@ private:
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PointCloudFilterNode>());
+    rclcpp::spin(std::make_shared<PointCloudAccumulator>());
     rclcpp::shutdown();
     return 0;
 }
